b8b2ad470b968278e793d901b24c9c55e40b8e24
DeanS <20067691@mail.wit.ie>
2025-04-18 16:28:24 +0100

    Improved custom Cognito authentication flow (SignUp, Confirm, SignIn, SignOut, Home updates)

--- src/auth/cognitoConfig.js
+++
@@ -1,7 +0,0 @@
-// export const cognitoAuthConfig = {
-//     authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_NZqc27Yt8",
-//     client_id: "2617dljqu24q3r5vttdfnmpce9",
-//     redirect_uri: "http://localhost:5173",
-//     response_type: "code",
-//     scope: "openid email profile",
-//   };
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from '../pages/Home';
 import SignUp from '../pages/SignUp';
 import ConfirmAccount from '../pages/ConfirmAccount';
+import SignIn from '../pages/SignIn';
 
 
 export default function AppRoutes() {
@@ -11,7 +12,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/confirm" element={<ConfirmAccount />} />
-        {/* Future Routes here like /signin /signup /forum */}
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
