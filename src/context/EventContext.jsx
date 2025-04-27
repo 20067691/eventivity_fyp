f06d1388d746fb2a9b1208b1a063ff6fa789956f
DeanS <20067691@mail.wit.ie>
2025-04-27 01:51:33 +0100

    Fixed page fail when refreshing

--- src/context/EventContext.jsx
+++ src/context/EventContext.jsx
@@ -1,10 +1,21 @@
 //EventContext.jsx
-import { createContext, useContext, useState } from 'react';
+import { createContext, useEffect, useContext, useState } from 'react';
 
 const EventContext = createContext();
 
 export function EventProvider({ children }) {
-  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(() => { 
+  const storedEvent = localStorage.getItem('selectedEvent');
+  return storedEvent ? JSON.parse(storedEvent) : null;
+});
+
+useEffect(() => {
+    if (selectedEvent) {
+      localStorage.setItem('selectedEvent', JSON.stringify(selectedEvent));
+    } else {
+      localStorage.removeItem('selectedEvent');
+    }
+  }, [selectedEvent]);
 
   return (
     <EventContext.Provider value={{ selectedEvent, setSelectedEvent }}>
